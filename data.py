1ca20aa216ea80b0218d23cb52aed980465c7a4b
Rigel <rydgel.code@gmail.com>
2021-10-26 14:24:29 -0400

    annoying debugging with cesar

--- data.py
+++ data.py
@@ -219,6 +219,7 @@ def read_michael_scan(filename, kwargs):
     ys = np.linspace(float(scan['Ystart (V)']), float(scan['Ystop (V)']), int(scan['Ypoints']))
     scan['Vxs'] = xs
     scan['Vys'] = ys
+    print(scan)
     return scan
 
 def load_2d_scan(filename, head):
